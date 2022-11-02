561b3ee51fbc3c1e16d2c4fbc889e14828f78a12
Peterson Larson <peter-larson@hotmail.com>
2022-11-02 00:20:55 -0300

    feature/dotenv_config

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,14 +2,17 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "./entity/User";
 import { Color } from "./entity/Color";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "root",
-    database: "Users",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
     entities: [User, Color],
